0d6654faa88372e9f941814168829b168a84ffaf
stepa <stepa03102007@gmail.com>
2023-03-19 20:51:16 +0300

    Добавил в настройки кнопку включения и выключения звука

--- Game.py
+++ Game.py
@@ -20,6 +20,8 @@ run = True
 
 crashed = False
 
+Sound_on = True
+
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
@@ -63,10 +65,11 @@ class ButonSet(pygame.sprite.Sprite):
         self.rect.topleft = (50, 250)
 
     def get_event(self, event):
-        global run, X, Sett
+        global run, X, Sett, Sound
         if self.rect.collidepoint(event.pos):
             X = ButonX(start_sprites)
             Sett = Sett_wind(start_sprites)
+            Sound = SoundButton(start_sprites)
 
 
 class ButonExit(pygame.sprite.Sprite):
@@ -84,27 +87,32 @@ class ButonExit(pygame.sprite.Sprite):
             run = False
 
 
-# class SoundButton(pygame.sprite.Sprite):
-#     image = load_image('Bt_X.png', color_key=-1)
-#     image2 = load_image('S_OFF.png', color_key=-1)
-#     image3 = load_image('S_ON.png', color_key=-1)
-#
-#     def __init__(self, group):
-#         super().__init__(group)
-#         self.image = SoundButton.image
-#         self.rect = self.image.get_rect()
-#         self.rect.topleft = (700, 500)
-#
-#     def get_event(self, event):
-#         global run
-#         if self.rect.collidepoint(event.pos):
-#             if pygame.mixer.music.get_volume() > 0:
-#                 pygame.mixer.music.set_volume(0)
-#                 self.image = self.image2
-#
-#             else:
-#                 pygame.mixer.music.set_volume(10)
-#                 self.image = self.image3
+class SoundButton(pygame.sprite.Sprite):
+    image = load_image('S_ON.png', color_key=-1)
+    image2 = load_image('S_OFF.png', color_key=-1)
+    image3 = load_image('S_ON.png', color_key=-1)
+
+    def __init__(self, group):
+        super().__init__(group)
+        if Sound_on == True:
+            self.image = SoundButton.image
+        else:
+            self.image = self.image2
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (525, 400)
+
+    def get_event(self, event):
+        global run, Sound_on
+        if self.rect.collidepoint(event.pos):
+            if pygame.mixer.music.get_volume() > 0:
+                pygame.mixer.music.set_volume(0)
+                self.image = self.image2
+                Sound_on = False
+
+            else:
+                pygame.mixer.music.set_volume(10)
+                self.image = self.image3
+                Sound_on = True
 
 
 class ButonX(pygame.sprite.Sprite):
@@ -121,6 +129,7 @@ class ButonX(pygame.sprite.Sprite):
         if self.rect.collidepoint(event.pos):
             X.kill()
             Sett.kill()
+            Sound.kill()
 
 
 class Sett_wind(pygame.sprite.Sprite):
